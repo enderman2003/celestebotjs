6495355000218c75b2e712bbf1f6bcb98f8df4c2
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-25 00:15:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.login(process.env.TOKEN)
+client.login("MTA1MTcyMjk3Mzk3Njk5Mzg1Mg.GoLeSF.aR8HN9iV3-mNaCF6lOu75SUwYxig54Frnjsf9w")
