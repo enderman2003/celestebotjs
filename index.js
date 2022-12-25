358e6412e4953b0973800294d09c552033fc5135
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-25 23:00:45 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.login("MTA1MTcyMjk3Mzk3Njk5Mzg1Mg.GoLeSF.aR8HN9iV3-mNaCF6lOu75SUwYxig54Frnjsf9w")
