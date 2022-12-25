1f8b0032cb770c0fcdf73ce87d6c05985548b590
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-25 23:22:33 +0530

    Update b.js

---
+++ Components/b.js
@@ -0,0 +1,35 @@
+const _supabase = require('@supabase/supabase-js')
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder, ButtonStyle} = require("discord.js")
+const SUPABASE_URL = 'https://dxflwfledezyinanacmg.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4Zmx3ZmxlZGV6eWluYW5hY21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk2OTczMzksImV4cCI6MTk4NTI3MzMzOX0.2aWmdFYDY_SBTMwNT1zeOGv-R_5uuBZEoVS9RxNCNaI'
+// Create a single supabase client for interacting with your database
+const supabase = _supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+const WAIFU_CHANNEL = "1056425420746141708"
+var bidderId = 0
+
+module.exports = {
+    name: "b",
+    description: "Bids against other user",
+    async execute(message, args, client, amount) {
+        const { data, error } = await supabase.auth.getSession()
+        if(error==null && bidderId != message.author.id){
+            bidderId = message.author.id
+            registerEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Bid Placed Successfully')
+            .setDescription('Bid of ${amount} has been placed successfully by ${client.author.name}')
+
+            channel = await client.channels.fetch(WAIFU_CHANNEL)
+            channel.send({ embeds: [registerEmbed] })
+        }
+        else{
+            errEmbed = new EmbedBuilder()
+            .setColor(0xFF0000)
+            .setTitle('Registered Already')
+            .setDescription('Bid is already placed by you.')
+
+            channel = await client.channels.fetch(WAIFU_CHANNEL)
+            channel.send({ embeds: [errEmbed] })
+        }
+    }
+}
