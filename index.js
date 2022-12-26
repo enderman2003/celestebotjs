2ecab58ca5b33710faf562666b7e6df495fa12a9
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-26 22:59:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,39 @@ client.on('messageCreate', async msg => {
 });
 
 client.on('guildMemberAdd', member => {
+    const { data, error } = await supabase
+    .from('Gifs')
+    .select('url')
+    .eq('type', 'WELCOME')
     welcomeEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
 	.setTitle('Some title')
-	.setDescription('Some description here')
-	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setDescription(`
+		ˏˋ°•⁀➷
+		୨୧ ` + member.user + ` Welcome new Owl🎐
+		*・✦ ⊹₊꒷˚૮₍｡• – •｡₎ა˚ ꒷꒦
+		ʚ🍧:Please read the rules‹3
+		⊹₊꒷︶︶꒷︶︶꒷꒦︶︶꒦‧🌸
+		ʚ💌ɞ _Read the rules at: ➷
+		ೃ༄--<#1005894981891920026>-
+		୨verify by doing Wick bots verification
+		which you can find underneath the rules !!!* ୨ 🍣
+		✦ ₊꒷꒦︶︶︶ ୨୧ ︶︶︶꒷꒦ෆ
+		ʚ💌ɞ _Chat at: ➷
+		₊·͟͟͞➳❥<#1006272345893240882>-
+		✦ ₊꒷꒦︶︶︶ ୨୧ ︶︶︶꒷꒦ෆ
+		ʚ💌ɞ _Selfroles at: ➷
+		・❥<#1005896784322773013>-
+		✦ ₊꒷꒦︶︶︶ ୨୧ ︶︶︶꒷꒦ෆ
+		ʚ💌ɞ _Introduction at: ➷
+		・❥<#1005931970007670845>-
+		︶︶꒷₊˚๑꒦🧁︶꒷꒦꒦︶꒷✦🍨
+		ʚ Hope you like it and have fun on our server! Feel at ease!꒷꒦🍭
+		୨ ╭ ୨୧ ✦ ︶꒷꒦・⎯⎯
+		┇・ ˎˊ Welcome to C e l e s t e <3
+		୨ ╰ ✦ ・⎯⎯・⎯⎯・꒷꒦ ♡ˎˊ˗
+	`)
+	.setImage(data[0].url[0])
 	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
     client.channels.get(`channelID`).send({ embeds: [welcomeEmbed] })
@@ -42,7 +70,12 @@ client.on('guildMemberRemove', member => {
     goodbyeEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
 	.setTitle('Some title')
-	.setDescription('Some description here')
+	.setDescription(`
+		୨🍡୧ *Goodbye, ` + member.user + ` fellow scholar
+		˚꒷꒷︶₊˚૮₍｡• – •｡₎ა˚₊︶꒷꒷˚
+		I hope that one day you change your mind and come back to our server! 🍧๑ Well, now there's no way to go back... Now we'll have to go on without them! 🍭
+		꒷꒷︶₊˚૮₍｡• – •｡₎ა˚₊︶꒷꒷˚
+    	`)
 	.setImage('https://i.imgur.com/AfFp7pu.png')
 	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
