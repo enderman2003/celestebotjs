3ecdd0033b692167a74b5adf715956caae2c506a
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-29 18:56:29 +0530

    Update b.js

--- Components/b.js
+++ Components/b.js
@@ -22,6 +22,7 @@ export async function b(message, args, client) {
                 return true
             }
             set_globals('bidderId', message.author.id)
+            set_globals('bidAmt', args[0])
             var registerEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle('Bid Placed Successfully')
@@ -29,7 +30,7 @@ export async function b(message, args, client) {
 
             var channel = await client.channels.fetch(WAIFU_CHANNEL)
             channel.send({ embeds: [registerEmbed] })
-            bid_timer(client)
+            bid_timer(message, client)
         }
         else {
             var errEmbed = new EmbedBuilder()
