46757c347c1a42765ad4cf89591420bf27fa012f
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-29 12:39:02 +0530

    Update globals.js

--- Global/globals.js
+++ Global/globals.js
@@ -1,50 +1,62 @@
 const globals = {
     auctionProcess: {
-      value: false
+        value: false
     },
     bidAmt: {
-      value: 0
+        value: 0
     },  
     hostId: {
-      value: 0
+        value: 0
     },
     bidderId: {
-      value: 0
+        value: 0
     },
     timeoutSec: {
-      value: 25,
-      protected: true
+        value: 25,
+        protected: true
     },
-  }
-  
-  var timer;
+    auctionTimeout: {
+        value: 60,
+        protected: true
+    },
+}
 
-  export function get_globals(global) {
-    // return variable or false if not exists
+var timer;
+export function get_globals(global) {
+// return variable or false if not exists
     return globals[global] && globals[global].value ? globals[global].value : false;
-  };
-  
-  export function set_globals(global, value) {
-    // exists and is protected: return false
+};
+
+export function set_globals(global, value) {
+// exists and is protected: return false
     if (globals[global] && globals[global].protected && globals[global].protected === true)
       return false;
     // set global and return true
     globals[global] = { value: value };
     return true;
-  };
-  
-  export function bid_expired() {
+};
+
+class Timer {
+  constructor(timeout, callBack) {
+     this.timeout = timeout
+     this.callBack = callBack()
+  }
+  timer_start() {
+     timer = setTimeout(this.callBack, this.timeout * 1000)
+  }
+  timer_end() {
+     clearTimeout(this.timeout);
+  }
+}
+
+class bidAllType {
+  constructor() {
      set_globals('auctionProcess', false)
      set_globals('bidderId', 0)
      set_globals('bidAmt', 0)
      set_globals('hostId', 0)
+
      return true
   }
-
-  export function timer_start() {
-      timer = setTimeout(bidWon(), get_globals('timeoutSec') * 1000)
-  }
-
-  export function timer_end() {
-      clearTimeout(timer);
-  }
+}
+  
