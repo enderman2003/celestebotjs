7543803e16cdf3e3b1829fc3f72dd6b90fb97424
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-12-29 12:13:47 +0530

    Update globals.js

--- Global/globals.js
+++ Global/globals.js
@@ -12,10 +12,13 @@ const globals = {
       value: 0
     },
     timeoutSec: {
-      value: 25
+      value: 25,
+      protected: true
     },
   }
   
+  var timer;
+
   export function get_globals(global) {
     // return variable or false if not exists
     return globals[global] && globals[global].value ? globals[global].value : false;
@@ -31,9 +34,17 @@ const globals = {
   };
   
   export function bid_expired() {
-     globals['auctionProcess'] = false
-     globals['bidderId'] = 0
-     globals['bidAmt'] = 0
-     globals['hostId'] = 0
+     set_globals('auctionProcess', false)
+     set_globals('bidderId', 0)
+     set_globals('bidAmt', 0)
+     set_globals('hostId', 0)
      return true
+  }
+
+  export function timer_start() {
+      timer = setTimeout(bidWon(), get_globals('timeoutSec') * 1000)
+  }
+
+  export function timer_end() {
+      clearTimeout(timer);
   }
