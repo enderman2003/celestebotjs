96e41517ede434b1cf6379606bcac1f225ab050e
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-01 09:44:04 +0530

    Update v.js

--- Components/v.js
+++ Components/v.js
@@ -1,41 +1,33 @@
-import { paginationEmbed } from 'discordjs-button-pagination';
+import { pagination, TypesButtons, StylesButton } from '@devraelfreeze/discordjs-pagination';
 
 import { EmbedBuilder , ButtonBuilder, ButtonStyle } from 'discord.js';
 
 export function v() {
-  const embed1 = new EmbedBuilder()
-                .setTitle('First Page')
-                .setDescription('This is the first page');
-
-  const embed2 = new EmbedBuilder()
-                .setTitle('Second Page')
-                .setDescription('This is the second page');
-
-  const button1 = new ButtonBuilder()
-                .setCustomId('previousbtn')
-                .setLabel('Previous')
-                .setStyle(ButtonStyle.Primary);
-
-  const button2 = new ButtonBuilder()
-                .setCustomId('nextbtn')
-                .setLabel('Next')
-                .setStyle(ButtonStyle.Primary);
-
-
-  pages = [
-	embed1,
-	embed2,
-	//....
-	//embedN
-  ];
-
-
-
-  buttonList = [
-    button1,
-    button2
-  ]
-
-
-  paginationEmbed(interaction, pages, buttonList, timeout);
+  await pagination({
+    interaction: interaction,
+    embeds: arrayEmbeds, // Array of embeds objects
+    author: interaction.member.user,
+    time: 40000, // 40 seconds
+    fastSkip: false,
+    disableButtons: true,
+    pageTravel: false,
+    /** Enable buttons pagination system only for member with ID: 123456789 */
+    customFilter: (interaction) => {
+        return interaction.member.user.id === '123456789';
+    },
+    buttons: [
+        {
+            value: TypesButtons.previous,
+            label: 'Previous Page',
+            style: StylesButton.Success,
+            emoji: '⏮️'
+        },
+        {
+            value: TypesButtons.next,
+            label: 'Next Page',
+            style: StylesButton.Success,
+            emoji: '⏩' // Disable emoji for this button
+        }
+     ]
+  });
 }
