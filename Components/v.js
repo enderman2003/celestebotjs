5991b2d15e7f63f483d9d5a41246d23daf4a9b67
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-01 19:52:28 +0530

    Update v.js

--- Components/v.js
+++ Components/v.js
@@ -17,7 +17,7 @@ export function v(msg, pages) {
   const col = msg.channel.createMessageCollector({ componentType: ComponentType.Button,  time: 20000 })
 
   col.on("collect", button => {
-    if (button.user.id !== msg.author.id) {
+    if (button.member.id !== msg.author.id) {
       return
     }
     if (button.id == "prevbtn") {
