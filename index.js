b72e5ea41fce1694b72e463151678255db311c45
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-01 14:31:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on('messageCreate', async msg => {
             var data = await supabase.from("Discord minigame").select("claimed_waifus").eq("dis_id", msg.author.id)
             console.log(data)
             var msgEmbed = []
-            for(int i=0; i<=data[0].length; i++) {
+            for(let i=0; i<=data[0].length; i++) {
               msgEmbed.push(new EmbedBuilder().setTitle("Claimed Assets").setImage("https://dxflwfledezyinanacmg.supabase.co/storage/v1/object/public/animenft/" + data[0].claimed_waifus[i]))
             }
             v(msg, msgEmbed)
