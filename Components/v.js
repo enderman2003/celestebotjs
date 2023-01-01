8a54464afca093e86c426823cf8b698ce0773fab
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-01 12:36:41 +0530

    Update v.js

--- Components/v.js
+++ Components/v.js
@@ -1,12 +1,21 @@
 import { pagination, TypesButtons, StylesButton } from '@devraelfreeze/discordjs-pagination';
 
-import { EmbedBuilder , ButtonBuilder, ButtonStyle } from 'discord.js';
+import { EmbedBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 
-export async function v() {
+export async function v(msg) {
+  var embed1 = new EmbedBuilder()
+  .setTitle("Test")
+  .setDescription()
+  var embed2 = new EmbedBuilder()
+  .setTitle("Test")
+  .setDescription()
+  var arrayEmbeds = [
+    embed1,
+    embed2
+  ]
   await pagination({
-    interaction: interaction,
     embeds: arrayEmbeds, // Array of embeds objects
-    author: interaction.member.user,
+    author: msg.author,
     time: 30000, // 30 seconds
     fastSkip: false,
     disableButtons: true,
@@ -23,7 +32,7 @@ export async function v() {
             value: TypesButtons.next,
             label: 'Next Page',
             style: StylesButton.Success,
-            emoji: '⏩' // Disable emoji for this button
+            emoji: '⏭️' // Disable emoji for this button
         }
      ]
   });
