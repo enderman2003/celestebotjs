83c5f486ffd148eca1e5c78f9a576cc5da1bee8d
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-01 17:43:45 +0530

    Update v.js

--- Components/v.js
+++ Components/v.js
@@ -13,7 +13,7 @@ export async function v(msg, pages) {
 			.setLabel('Previous')
 			.setStyle(ButtonStyle.Primary),
 	   );
-  const currPage = msg.channel.send({ embeds: pages[page], component: row })
+  const currPage = msg.channel.send({ embeds: [pages[page]], component: row })
   const filter = (b) => ["nextbtn", "prevbtn"].includes(b.id)
   const col = await currPage.createButtonCollector(filter, { time: ms('10s') })
 
@@ -28,6 +28,6 @@ export async function v(msg, pages) {
     else if(button.id == "nextbtn") {
       page = page + 1 < page.length ? page++ : 0;
     }
-    currPage.edit({ embeds: pages[page], component: row }) 
+    currPage.edit({ embeds: [pages[page]], component: row }) 
   });
 }
