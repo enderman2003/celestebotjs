735f88df4dc207c4c2a4879e1de0e86aa50ae9f7
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-02 21:07:07 +0530

    Update ha.js

--- Components/ha.js
+++ Components/ha.js
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { get_globals, set_globals, auction_timer } from "../Global/globals.js"
 import { Client, EmbedBuilder, GatewayIntentBits, userMention } from "discord.js"
-const SUPABASE_URL = 'https://dxflwfledezyinanacmg.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4Zmx3ZmxlZGV6eWluYW5hY21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk2OTczMzksImV4cCI6MTk4NTI3MzMzOX0.2aWmdFYDY_SBTMwNT1zeOGv-R_5uuBZEoVS9RxNCNaI'
-// Create a single supabase client for interacting with your database
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_KEY = process.env.SUPABASE_KEY
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 const WAIFU_CHANNEL = "1056425420746141708"
 var j = 0
