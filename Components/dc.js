f1133de6e0ec0f9b9410f148ca3564bcb4b652e7
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-04 00:46:30 +0530

    Update dc.js

--- Components/dc.js
+++ Components/dc.js
@@ -7,6 +7,31 @@ const SUPABASE_KEY = process.env.SUPABASE_KEY
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 const WAIFU_CHANNEL = "1056425420746141708"
 
+
 export async function dc(message, client) {
+  const rndInt = randomIntFromInterval(500, 1500)
+  const { data, error } = await supabase
+    .from('Discord minigame')
+    .select('*')
+    .eq('dis_id', message.author.id.toString())
   
+  var amount = data[0].amt + rndInt
+    
+  const { dat, err } = await supabase
+    .from('Discord minigame')
+    .update({ 'amt': amount })
+    .eq('dis_id', message.author.id.toString())
+    
+  var wonEmbed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Claimed Daily')
+        .setDescription(`Claimed daily for today by ${message.author.username}. \n Amount ${rndInt} :coin: successfully credited. \n Total Balance: ${data[0].amt} :coin:`)
+        .setFooter({ text: message.author.username });
+  
+  var channel = await client.channels.fetch(WAIFU_CHANNEL)
+  channel.send({ embeds: [wonEmbed] })
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
