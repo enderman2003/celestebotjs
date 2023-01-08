6b1d87cf150ac8120fc7d774c3c149cc7aa917c8
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-08 15:54:39 +0530

    Update dc.js

--- Components/dc.js
+++ Components/dc.js
@@ -9,27 +9,40 @@ const WAIFU_CHANNEL = process.env.WAIFU_CHANNEL
 
 
 export async function dc(message, client) {
-  const rndInt = randomIntFromInterval(500, 1500)
-  const { data, error } = await supabase
-    .from('Discord minigame')
-    .select('*')
-    .eq('dis_id', message.author.id.toString())
-  
-  var amount = data[0].amt + rndInt
-    
-  const { dat, err } = await supabase
-    .from('Discord minigame')
-    .update({ 'amt': amount })
-    .eq('dis_id', message.author.id.toString())
-    
-  var wonEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle('Claimed Daily')
-        .setDescription(`Claimed daily for today by ${message.author.username}. \nAmount ${rndInt} :coin: successfully credited. \nTotal Balance: ${amount} :coin:`)
-        .setFooter({ text: message.author.username });
-  
-  var channel = await client.channels.fetch(WAIFU_CHANNEL)
-  channel.send({ embeds: [wonEmbed] })
+  const user = await supabase.auth.user()
+  if(user==null && get_globals('auctionProcess') == 'false') { 
+    const rndInt = randomIntFromInterval(500, 1500)
+    const { data, error } = await supabase
+      .from('Discord minigame')
+      .select('*')
+      .eq('dis_id', message.author.id.toString())
+
+    var amount = data[0].amt + rndInt
+
+    const { dat, err } = await supabase
+      .from('Discord minigame')
+      .update({ 'amt': amount })
+      .eq('dis_id', message.author.id.toString())
+
+    var wonEmbed = new EmbedBuilder()
+          .setColor(0x0099FF)
+          .setTitle('Claimed Daily')
+          .setDescription(`Claimed daily for today by ${message.author.username}. \nAmount ${rndInt} :coin: successfully credited. \nTotal Balance: ${amount} :coin:`)
+          .setFooter({ text: message.author.username });
+
+    var channel = await client.channels.fetch(WAIFU_CHANNEL)
+    channel.send({ embeds: [wonEmbed] })
+  }
+  else {
+    var wonEmbed = new EmbedBuilder()
+          .setColor(0x0099FF)
+          .setTitle('User Not Found')
+          .setDescription(`You need to register first`)
+          .setFooter({ text: message.author.username });
+
+    var channel = await client.channels.fetch(WAIFU_CHANNEL)
+    channel.send({ embeds: [wonEmbed] })
+  }
 }
 
 function randomIntFromInterval(min, max) { // min and max included 
