eca166fcb821845095cd9bc86b28ad22c0244a9d
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-17 23:16:50 +0530

    Update v.js

--- Components/v.js
+++ Components/v.js
@@ -1,33 +1,17 @@
-import { ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ComponentType } from 'discord.js';
-export function v(msg, pages) {
-  let page = 0;
-  const row = new ActionRowBuilder()
-           .addComponents(
-		new ButtonBuilder()
-			.setCustomId('nextbtn')
-			.setLabel('Next')
-			.setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-			.setCustomId('prevbtn')
-			.setLabel('Previous')
-			.setStyle(ButtonStyle.Primary),
-	   );
-  const currPage = msg.reply({ embeds: [pages[0]], components: [row] })
-  const filter = (b) => ["nextbtn", "prevbtn"].includes(b.id)
-  const col = msg.channel.createMessageCollector({ componentType: ComponentType.Button,  time: 20000 })
+import { Pagination } from "@acegoal07/discordjs-pagination";
 
-  col.on("collect", button => {
-    if (button.member.id !== msg.author.id) {
-      return
-    }
-    if (button.id == "prevbtn") {
-      page = page > 0 ? --page : pages.length - 1   
-      button.deferUpdate()
-    }
-    else if(button.id == "nextbtn") {
-      page = page + 1 < pages.length ? page++ : 0;
-      button.deferUpdate()
-    }
-    msg.editReply({ embeds: [pages[page]] })
-  });
+export async function v(message, embeds){
+  new Pagination().setPortal(message)
+   .setPageList(embeds)
+   .setButtonList([
+      new ButtonBuilder()
+         .setLabel(`Previous`)
+         .setStyle("Secondary")
+         .setCustomId(`1`),
+      new ButtonBuilder()
+         .setLabel(`Next`)
+         .setStyle("Secondary")
+         .setCustomId(`2`)
+   ])
+   .paginate()
 }
