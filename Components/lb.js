bb40b9b4c440178ef56ab8087445078c93132e90
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-17 20:33:20 +0530

    Update lb.js

--- Components/lb.js
+++ Components/lb.js
@@ -9,8 +9,10 @@ const WAIFU_CHANNEL = process.env.WAIFU_CHANNEL
 
 
 export async function lb(message, client) {
-  const { data: { user } } = await supabase.auth.getUser()
-  console.log(user)
+  const { user, err } = await supabase
+        .from("Discord minigame")
+        .select("email")
+        .eq("dis_id", message.author.id)
   if (user !== null) {
     const rndInt = randomIntFromInterval(50, 350)
     const { data, error } = await supabase
