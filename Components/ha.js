e41e39c2de6a63ae749515fc325d8ff2fde063ef
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-18 08:23:01 +0530

    Update ha.js

--- Components/ha.js
+++ Components/ha.js
@@ -5,7 +5,8 @@ const SUPABASE_URL = process.env.SUPABASE_URL
 const SUPABASE_KEY = process.env.SUPABASE_KEY
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 const WAIFU_CHANNEL = process.env.WAIFU_CHANNEL
-var j = 0
+var gen_desc = "Gen 1"
+var rarity_color = 0xb3b3b3
 
 export async function ha(message, client) {
   if(get_globals('auctionProcess') == 'false') { 
@@ -23,19 +24,20 @@ export async function ha(message, client) {
           if (data[j].name != i[j]) {
             set_globals('imgHash', data[j].name)
             console.log(data[j].name)
+            if(data[j].name.substring(0, 4) == "gen2") { gen_desc = "Gen2"; rarity_color = 0x57ffa5; }
             break;
           }
-          j++
         }
       }
     }
     else { set_globals('imgHash', data[Math.floor(Math.random() * data.length) + 1].name) }
     console.log(get_globals('imgHash'))
     var auctionEmbed = new EmbedBuilder()
-    .setColor(0x0099FF)
+    .setColor(rarity_color)
     .setTitle('Auction Started')
-    .setDescription("For today's auction we have this beauty")
+    .setDescription(`For today's auction we have this beauty`)
     .setImage('https://dxflwfledezyinanacmg.supabase.co/storage/v1/object/public/animenft/' + get_globals('imgHash'))
+    .setFooter({ text: gen_desc })
 
     var channel = await client.channels.fetch(WAIFU_CHANNEL)
     channel.send({ embeds: [auctionEmbed] })
